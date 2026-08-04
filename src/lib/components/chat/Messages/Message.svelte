708400bbd05b51d98635c5380db78232cba4395e
NguyenVanHung2004 <22028118@vnu.edu.vn>
2026-08-04 13:01:53 +0700

    fix: remove default white hover on whole message area

--- src/lib/components/chat/Messages/Message.svelte
+++ src/lib/components/chat/Messages/Message.svelte
@@ -57,9 +57,7 @@
 	role="listitem"
 	class="flex flex-col justify-between px-3.5 mb-3 w-full {($settings?.widescreenMode ?? null)
 		? 'max-w-full'
-		: 'max-w-[58rem]'} mx-auto rounded-lg group hover:bg-white/50 dark:hover:bg-white/[0.03] transition-colors {isSafari
-		? ''
-		: 'message-listitem'}"
+		: 'max-w-[58rem]'} mx-auto rounded-lg group {isSafari ? '' : 'message-listitem'}"
 >
 	{#if history.messages[messageId]}
 		{#if history.messages[messageId].role === 'user'}
